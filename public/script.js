83fa0fe650edf1d4a9d33ad924ba68df6b0362b9
Raul <sarmancoder@gmail.com>
2026-03-18 17:11:35 +0100

    cambiar proyecto de next a vite

--- public/script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hola desde el script<')
-
-window.addEventListener("beforeinstallprompt", (event) => {
-    console.log('before install')
-    event.preventDefault();
-    window.installPrompt = event
-});
